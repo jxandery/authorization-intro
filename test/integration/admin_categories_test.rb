01ec1725fbea959e3950c06d79306972b4716e21
Jack Yeh <jack.yeh@gmail.com>
2015-06-10 18:46:49 -0600

    Add test: default user doesn't have admin privileges: - the existing test is sufficient, as it ensures admin can see what it is supposed to see - now let's test that a non-admin doesn't have admin privileges   + we can do this by making sure that a non-admin doesn't see something an admin would see, 'All Categories'   + we can also make sure that they get an error message, vis-a-vis the 'app/public/404' message

--- test/integration/admin_categories_test.rb
+++ test/integration/admin_categories_test.rb
@@ -11,4 +11,15 @@ class AdminCategoriesTest < ActionDispatch::IntegrationTest
     visit admin_categories_path
     assert page.has_content?("All Categories")
   end
+
+  test 'default user does not see admin categories index' do
+    user = User.create(username: "default_user",
+                       password: "password",
+                       role: 0)
+
+    ApplicationController.any_instance.stubs(:current_user).returns(user)
+    visit admin_categories_path
+    refute page.has_content?("All Categories")
+    assert page.has_content?("The page you were looking for doesn't exist.")
+  end
 end
